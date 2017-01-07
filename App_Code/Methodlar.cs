42ed90a6c05777b9dcb079a81a8bd00720b22f67
Mahmut Yıldız <yldzmahmut0@gmail.com>
2017-01-07 23:02:34 +0200

    Appcode css bat dosyalarını oluşturuldu

---
+++ App_Code/Methodlar.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Methodlar
+/// </summary>
+public class Methodlar
+{
+    public Methodlar()
+    {
+    }
+
+    public SqlConnection baglan()
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-V12B8LA\\SQLEXPRESS; Initial Catalog=PakProfil; Integrated Security=true");
+        baglanti.Open();
+        return (baglanti);
+    }
+
+    public int cmd(string sqlcumle)
+    {
+        SqlConnection baglanti = this.baglan();
+        SqlCommand cmd = new SqlCommand(sqlcumle, baglanti);
+        int sonuc = 0;
+        try
+        {
+            sonuc = cmd.ExecuteNonQuery();
+
+        }
+        catch (SqlException ex)
+        {
+
+            throw new Exception(ex.Message + "(" + sqlcumle + ")");
+        }
+
+        cmd.Dispose();
+        baglanti.Close();
+        baglanti.Dispose();
+        return sonuc;
+
+
+    }
+
+    public DataTable GetDataTable(string sqlcumle)
+    {
+        SqlConnection baglanti = this.baglan();
+        SqlDataAdapter dap = new SqlDataAdapter(sqlcumle, baglanti);
+
+        DataTable dt = new DataTable();
+
+        try
+        {
+            dap.Fill(dt);
+
+        }
+        catch (SqlException ex)
+        {
+
+            throw new Exception(ex.Message + "(" + sqlcumle + ")");
+        }
+
+        dap.Dispose();
+        baglanti.Close();
+        baglanti.Dispose();
+
+        return dt;
+    }
+
+    public DataRow GetDataRow(string sqlcumle)
+    {
+        DataTable dt = GetDataTable(sqlcumle);
+        if (dt.Rows.Count == 0)
+        {
+            return null;
+        }
+        else
+        {
+            return dt.Rows[0];
+        }
+    }
+
+    public string GetDataCell(string sqlcumle)
+    {
+        DataTable dt = GetDataTable(sqlcumle);
+        if (dt.Rows.Count == 0)
+        {
+            return null;
+        }
+        else
+        {
+            return dt.Rows[0][0].ToString();
+        }
+
+
+    }
+}
